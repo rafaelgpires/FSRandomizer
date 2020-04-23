0259abead337a4f831ad16320e35182084d9b993
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-04-23 19:21:54 +0100

    Update lists.sql
    Int to Varchar for uniqid()

--- SQL/lists.sql
+++ SQL/lists.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `lists`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `lists` (
-  `id` int NOT NULL,
+  `id` varchar(45) NOT NULL,
   `hash` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -38,4 +38,4 @@ CREATE TABLE `lists` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-23 15:22:02
+-- Dump completed on 2020-04-23 18:53:00
