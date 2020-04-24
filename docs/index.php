cf441ed1d98d6dc210633d046ad43830f190c6b3
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-04-24 04:19:51 +0100

    Output Hash from ID / Hardcoded Hash File
    Coded a GET request to index.php to make it output a hash.
    
    Added temp_hash.txt as a temporary hardcoded hash for the app.

--- docs/index.php
+++ docs/index.php
@@ -2,4 +2,12 @@
 include("includes/func_notepadTable.php");
 include("database.php");
 include("fslister.php");
+
+if(isset($_GET['uniqueID']) && isset($_GET['output'])) {
+	$list = new \Lister\FSLister();
+	$list->getList($_GET['uniqueID']);
+	switch($_GET['output']) {
+		case 'hash': die($list->hash); break;
+	}
+}
 ?>
