c16553968aa3fc4a669451b04560c2446552e22a
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-05-02 23:09:08 +0100

    Add Body from XD

--- docs/index.php
+++ docs/index.php
@@ -12,9 +12,9 @@ class HTML {
 	public function __construct($dir) {
 		$this->navbar = file_get_contents($dir . '/navbar.html');
 	}
-} $html = new HTML('html');
+} $html = new HTML('./html');
 
-//Parse input
+//Parse input: UniqueID
 if(isset($_GET['UniqueID'])) {
 	//UniqueID is given and output variable is set
 	if(isset($_GET['output'])) {
@@ -28,7 +28,15 @@ if(isset($_GET['UniqueID'])) {
 	} else require("fctracker.php"); //UniqueID is given but no output requested, load fctracker.php
 	
 	//Make sure no other code runs after this, without having to include it all in one big else
-	die();
+	exit;
+}
+
+//Parse input: Generate
+if(isset($_GET['generate'])) {
+	$list = new FSLister();
+	$list->createList();
+	echo json_encode($list->listID);
+	exit;
 }
 
 //Landing page
@@ -42,13 +50,69 @@ if(isset($_GET['UniqueID'])) {
 	<title>FSRandomizer</title>
 </head>
 <body>
-	<!-- Navbar -->
-	<?=$html->navbar?>
+	<!-- Content -->
+	<div class="container" style="height: 100vh">
+		<!-- Navbar -->
+		<div class="row h-25 pt-4 align-items-top">
+			<?=$html->navbar?>
+		</div>
+		
+		<!-- Generation -->
+		<div class="row h-50 align-items-center">
+			<div class="container mt-6">
+				<div class="row d-flex justify-content-center">
+					<div class="alert d-none" role="alert" id="alert"></div>
+				</div>
+				<div class="row d-flex justify-content-center">
+					<button id="generator" class="btn btn-primary px-4 py-0">GENERATE</button>
+				</div>
+				<div class="row mt-4 d-flex justify-content-center">
+					<h1 class="text-center">Randomize a full Guitar Hero career!<br/>
+					Revisit the classics, ROCK ON!</h1>
+				</div>
+				<div class="row mt-4 d-flex justify-content-center">
+					<a class="options"></a>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Links -->
+		<div class="row h-25 pb-5 align-items-end">
+			<div class="container">
+				<div class="row justify-content-around">
+					<div class="col-md-4 pb-5">
+						<div class="container">
+							<a href="#" target="_blank" class="link-unstyled">
+								<div class="row mb-2 text-nowrap">
+									<div class="col p-0 text-nowrap"><b>Download the Charts</b></div>
+									<div class="col p-0 text-right align-self-center d-none d-lg-block"><img src="./images/+.png" /></div>
+								</div>
+							</a>
+							<a href="#" target="_blank" class="link-unstyled"><div class="row text-nowrap"><p>Download The full GH Series Charts!</p></div></a>
+							<div class="row text-nowrap"><p>Click <a href="#" target="_blank">here</a> for DLC and <a href="#" target="_blank">here</a> for extra original content.</p></div>
+						</div>
+					</div>
+					<div class="col-md-4 pb-5">
+						<div class="container">
+							<a href="#" target="_blank" class="link-unstyled">
+								<div class="row mb-2 text-nowrap">
+									<div class="col p-0 text-nowrap"><b>Your list in Clone Hero</b></div>
+									<div class="col p-0 text-right align-self-center d-none d-lg-block"><img src="./images/+.png" /></div>
+								</div>
+							</a>
+							<div class="row text-nowrap"><p>Download our <a href="#" target="_blank">App</a> to bring your list to Clone Hero!</p></div>
+							<div class="row text-nowrap"><p>It's <a href="https://github.com/rafaelgpires/FSRandomizer-App/" target="_blank">Open Source</a> and requires using <a href="#" target="_blank">these charts</a>.</p></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 	<!-- jQuery, Popper, bootstrap.js, Local scripts -->
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script src="js/fsrandomizer.js"></script>
+	<script src="./js/fsrandomizer.js"></script>
 </body>
 </html>
--- docs/js/fsrandomizer.js
+++ docs/js/fsrandomizer.js
@@ -1,5 +1,20 @@
+//Alerts
+function setAlert(id, message=null, type=null) {
+	if(!message) { $('#'+id).hide(); }
+	else {
+		$("#"+id)
+			.text(message)
+			.attr('class', 'alert alert-' + type)
+			.show();
+	}
+}
+
+/* Landing Page
+ * No need to run a check on whether we're on the landing page, since jQuery does that check for us
+ * the code will simply not run if the elements don't exist without throwing any errors
+ */
 //NavBar: Input ID
-$("#InputIDForm").submit(function(event){
+$("#InputIDForm").submit(function(){
 	//Check validity of the ID
 	var continueSubmit = true;
 	$.ajax({
@@ -17,4 +32,29 @@ $("#InputIDForm").submit(function(event){
 	
 	//Either block the submit, or let it through
 	return continueSubmit;
+});
+
+//Generator: Button
+$("#generator").click(function(){
+	//Let the user know it's loading
+	setAlert('alert', 'Your list is being created...', 'info');
+	
+	//Generate a list with that ID
+	var listGenerated = false;
+	$.ajax({
+		dataType: "json",
+		data: { generate: null },
+		async: false,
+		success: function(data) {
+			//What to do after getting a reply
+			if(data) {
+				listGenerated = true;
+				setAlert('alert', ('Your list has been created: ' + data + '!'), 'success');
+				window.location = './' + data;
+			}
+		}
+	});
+	
+	//If this runs, then there was an unknown error
+	if(listGenerated == false) { setAlert('alert', 'Uh oh... There was an unknown error creating your list.', 'danger'); }
 });
