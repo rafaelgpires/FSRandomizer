496d15d5d313b2540ef60332ebfed089a2d752cd
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-05-06 00:38:33 +0100

    Local host dependencies
    Hosting servers were having some problems causing the page to take several seconds to load, switched to local.

--- docs/index.php
+++ docs/index.php
@@ -11,6 +11,8 @@ class HTML {
 	public $navbar, $scripts, $metalinks;
 	public function __construct($dir) {
 		$this->navbar = file_get_contents($dir . '/navbar.html');
+		$this->styles = file_get_contents($dir . '/styles.html');
+		$this->links  = file_get_contents($dir . '/links.html');
 	}
 } $html = new HTML('./html');
 
@@ -66,8 +68,7 @@ if(isset($_GET['http_error'])) {
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/fsrandomizer.css">
+	<?=$html->styles?>
 	<title>FSRandomizer</title>
 </head>
 <body>
@@ -217,9 +218,6 @@ if(isset($_GET['http_error'])) {
 	</div>
 	
 	<!-- jQuery, Popper, bootstrap.js, Local scripts -->
-	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script src="./js/fsrandomizer.js"></script>
+	<?=$html->links?>
 </body>
 </html>
